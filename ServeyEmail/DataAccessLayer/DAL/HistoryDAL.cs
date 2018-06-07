a9879b892433357718ec95f521596d4b473ab7a7
DoHue97 <dohue97yp1@gmai.com>
2018-06-07 16:33:13 +0700

    add DAL

---
+++ ServeyEmail/DataAccessLayer/DAL/HistoryDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.BaseDAL;
+using DataAccessLayer.Model;
+using ValueObjects;
+namespace DataAccessLayer.DAL
+{
+     public class HistoryDAL:BaseHistory<OHistories>
+    {
+        private StatusSurveyEntities db;
+        public HistoryDAL()
+        {
+            db = new StatusSurveyEntities();
+        }
+        public override List<OHistories> Getall()
+        {
+            List<OHistories> listHis = new List<OHistories>();
+            var list = db.History_Select_All();
+            foreach (var item in list)
+            {
+                OHistories his = new OHistories();
+                his.IdHis = item.IdHis;
+                his.IdStatus = item.IdStatus;
+                his.CreationDate = item.CreationDate;
+                his.Amount = item.Amount;
+                his.IdGroup = item.IdGroup;
+
+                listHis.Add(his);
+            }
+            return listHis;
+        }
+        public override bool Insert(OHistories his)
+        {
+            db.History_Insert(his.IdHis, his.CreationDate, his.IdStatus, his.IdGroup);
+            return true;
+        }
+        public override bool Update(OHistories his)
+        {
+            db.History_Update(his.IdHis);
+            return true;
+        }
+        public override bool Delete(OHistories his)
+        {
+            db.History_Delete(his.IdHis);
+            return true;
+        }
+    }
+}
---
+++ ServeyEmail/DataAccessLayer/DAL/StatusDAL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.BaseDAL;
+using DataAccessLayer.Model;
+using ValueObjects;
+namespace DataAccessLayer.DAL
+{
+    public class StatusDAL:BaseStatus<OStatus>
+    {
+        private StatusSurveyEntities db;
+        public StatusDAL()
+        {
+            db = new StatusSurveyEntities();
+        }
+        public override List<OStatus> Getall()
+        {
+            List<OStatus> listSta = new List<OStatus>();
+            var list = db.Status_Select_All();
+            foreach (var item in list)
+            {
+                OStatus stt = new OStatus();
+                stt.IdStatus = item.IdStatus;
+                stt.Name = item.Name;
+
+                listSta.Add(stt);
+            }
+            return listSta;
+        }
+        public override bool Insert(OStatus status)
+        {
+            db.Status_Insert(status.IdStatus, status.Name);
+            return true;
+        }
+        public override bool Rename(OStatus status)
+        {
+            db.Status_Rename(status.IdStatus, status.Name);
+            return true;
+        }
+        public override bool Delete(OStatus status)
+        {
+            db.Status_Delete(status.IdStatus);
+            return true;
+        }
+    }
+}
