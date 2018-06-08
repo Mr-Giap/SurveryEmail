5dbf3191f6ac7c4dc7a4a72df979c2a63e8ac1e4
ddoongvn <39513425+ddoongvn@users.noreply.github.com>
2018-06-08 09:55:23 +0700

    sd

---
+++ ServeyEmail/BusinessLogicLayer/BLL/StatusBLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicLayer.BaseBLL;
+using DataAccessLayer.DAL;
+using ValueObjects;
+
+namespace BusinessLogicLayer.BLL
+{
+    class StatusBLL : BaseStatus<OStatus>
+    {
+        StatusDAL stt = new StatusDAL();
+        public override List<OStatus> Getall()
+        {
+            return stt.Getall();
+        }
+        public override bool Delete(OStatus status)
+        {
+            return stt.Delete(status);
+        }
+        public override bool Insert(OStatus status)
+        {
+            return stt.Insert(status);
+        }
+        public override bool Rename(OStatus status)
+        {
+            return stt.Rename(status);
+        }
+
+    }
+}
