1702ac5adeb4ed8b303c1627ffd904677a5b4dcd
DoHue97 <dohue97yp1@gmai.com>
2018-06-08 17:10:03 +0700

    add account controller, views accout , edit layout

---
+++ ServeyEmail/ServeyEmail/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ValueObjects;
+
+namespace ServeyEmail.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (OUsers)Session[CommonConstant.SESSION_ACCOUNT];
+            if (session == null)
+            {
+                // Chưa đăng nhập => trang chủ khách hàng
+                filterContext.Result = new RedirectToRouteResult(new
+                        RouteValueDictionary(new
+                        {
+                            controller = "Logins",
+                            action = "Login"
+                        }));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
